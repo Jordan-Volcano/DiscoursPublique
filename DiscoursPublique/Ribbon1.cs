0c0a38ea43b1d268c579fa368a712f6f1b236eb5
Jordan Matas <j.matasfils@gmail.com>
2018-06-11 18:46:08 +0200

    button-1 action ok

--- DiscoursPublique/Ribbon1.cs
+++ DiscoursPublique/Ribbon1.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Microsoft.Office.Tools.Ribbon;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using Microsoft.Office.Interop.Excel;
 using System.Globalization;
+using System.Threading;
 
 namespace DiscoursPublique
 {
@@ -19,14 +18,15 @@ namespace DiscoursPublique
 
         private void button1_Click(object sender, RibbonControlEventArgs e)
         {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("fr-FR");
+
             var bosquejos = new Bosquejos();
             Workbook wb = Globals.ThisAddIn.GetActiveWorkbook();
 
             foreach (Excel.Worksheet sheet in wb.Worksheets)
             {
                 if (sheet.Name == "Bosquejos") {
-                    MessageBox.Show(sheet.Name);
-
+                    
                     Range rngBosquejos = sheet.get_Range("A5", "A236");
                     Range rngDateBosquejos = sheet.get_Range("C5", "C236");
 
@@ -43,7 +43,7 @@ namespace DiscoursPublique
                 }
 
                 else if(sheet.Name == "Hermanos") {
-                    MessageBox.Show(sheet.Name);
+                   
                 }
                 else { 
                     Range rngdiscours = sheet.get_Range("D13", "D36");
@@ -59,10 +59,7 @@ namespace DiscoursPublique
                     {
                         for (int j = 1; j < bosquejos.numeros.Length; j++) { 
 
-                            string num = bosquejos.numeros.ElementAt(j);
-
-
-                            if (discours[i] == num)
+                            if (discours[i] == bosquejos.numeros.ElementAt(j) )
                             {
                                                            
                                 DateTime dateDiscours = DateTime.Parse(dates[i], new CultureInfo("fr-FR")) ;
@@ -72,19 +69,28 @@ namespace DiscoursPublique
 
                                     bosquejos.datesBosquejo[j] = "01/01/2015";
                                 }
-                                
-                                    DateTime dateBosquejo = DateTime.Parse(bosquejos.datesBosquejo.ElementAt(j), new CultureInfo("fr-FR"));
-                               
+
                                 //TODO : Prendre en charge les valeur où il n'y a pas de date
 
-
-
+                                DateTime dateBosquejo = DateTime.Parse(bosquejos.datesBosquejo.ElementAt(j), new CultureInfo("fr-FR"));
+                               
+                             
 
                                if( dateDiscours > dateBosquejo)
                                 {
 
-                                    MessageBox.Show("la date programmé "+ dateDiscours.ToShortDateString() +" est supérieur à la date inscrite "+ dateBosquejo.ToShortDateString() +" pour le discours n° "+ num );
+
+                                    MessageBox.Show("la date programmé "+ dateDiscours.ToShortDateString() +" est supérieur à la date inscrite "+ dateBosquejo.ToShortDateString() +" pour le discours n° "+ discours[i] );
+
+                                    Worksheet sheet2 = wb.Sheets["Bosquejos"];
+
+                                    Range newrange = sheet2.get_Range("C" + (j + 5));
+                                    
+                                    newrange.Cells.Value = dateDiscours.ToShortDateString();
+
                                 }
+
+
                                 
                                 
 
